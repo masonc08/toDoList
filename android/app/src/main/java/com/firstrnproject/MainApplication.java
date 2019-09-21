9700dde05ddbaa163db755c479241c7a00bad2e8
Mason Cao <32713681+masonc08@users.noreply.github.com>
2019-09-21 12:31:53 -0400

    Added vector icon dependency and tasks display

--- android/app/src/main/java/com/firstrnproject/MainApplication.java
+++ android/app/src/main/java/com/firstrnproject/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.List;
 
